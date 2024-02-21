0c912f27fde50b76903209b85533f5664c0f2356
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-02-21 17:35:00 +0200

    Finish Header component unresponsive styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,22 @@ import ShoppingBagLineIcon from "remixicon-react/ShoppingBagLineIcon";
 import Heart from "remixicon-react/HeartLineIcon";
 import userImg from "../../assets/images/user-icon.png";
 import Menu from "remixicon-react/MenuLineIcon";
+import { motion } from "framer-motion";
+
+const links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/shop",
+    display: "Shop",
+  },
+  {
+    path: "/checkout",
+    display: "Checkout",
+  },
+];
 export default function Header() {
   return (
     <header className={styles.header}>
@@ -24,32 +40,41 @@ export default function Header() {
 
             <nav className={styles.navigation}>
               <ul className={styles.menu}>
-                <li className={styles.navItem}>
-                  <NavLink to="/home">Home</NavLink>
-                </li>
-                <li className={styles.navItem}>
-                  <NavLink to="/shop">Shop</NavLink>
-                </li>
-                <li className={styles.navItem}>
-                  <NavLink to="/checkout">Checkout</NavLink>
-                </li>
+                {links.map((link) => (
+                  <li className={styles.navItem} key={link.path}>
+                    <NavLink
+                      to={link.path}
+                      className={(navClass) =>
+                        navClass.isActive ? styles.active : ""
+                      }
+                    >
+                      {link.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
             </nav>
 
             <nav className={styles.navIcons}>
               <span className={styles.favIcon}>
-                <Heart />
+                <Heart className={styles.icon} />
+                <span className={styles.badge}>1</span>
               </span>
               <span className={styles.cartIcon}>
-                <ShoppingBagLineIcon />
+                <ShoppingBagLineIcon className={styles.icon} />
+                <span className={styles.badge}>1</span>
               </span>
               <span>
-                <img src={userImg} alt="user profile picture" />
+                <motion.img
+                  whileTap={{ scale: 1.2 }}
+                  src={userImg}
+                  alt="user profile picture"
+                />
               </span>
-              <div className={styles.mobileMenu}>
-                <Menu />
-              </div>
             </nav>
+            <div className={styles.mobileMenu}>
+              <Menu />
+            </div>
           </div>
         </Row>
       </Container>
