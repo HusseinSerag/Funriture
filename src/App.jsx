81bbaeee753f11c503803708003d0742129796a8
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-02-21 15:48:42 +0200

    Create default sass file and bring in data
    just regular boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function App() {
+  return (
+    <Container fluid="md">
+      <Row>
+        <Col>Hello</Col>
+        <Col>React</Col>
+      </Row>
+    </Container>
+  );
+}
