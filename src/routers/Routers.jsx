fbae59f2d4d9a62ea875eb0bc4acb48314ca9702
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-02-23 00:53:44 +0200

    Implementcode-splitting on page level

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,37 +1,43 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Shop from "../pages/Shop";
-import ProductDetails from "../pages/ProductDetails";
-import Register from "../pages/Register";
-import Cart from "../pages/Cart";
-import Login from "../pages/Login";
-import Checkout from "../pages/Checkout";
+import { Suspense, lazy } from "react";
+const Home = lazy(() => import("../pages/Home"));
+const Shop = lazy(() => import("../pages/Shop"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails"));
+const Register = lazy(() => import("../pages/Register"));
+const Cart = lazy(() => import("../pages/Cart"));
+
+const Login = lazy(() => import("../pages/Login"));
+
+const Checkout = lazy(() => import("../pages/Checkout"));
 import Description from "../components/Description/Description";
 import Reviews from "../components/Reviews/Reviews";
 import ProtectRoute from "./ProtectRoute";
+import SpinnerFullPage from "../components/SpinnerFullPage/SpinnerFullPage";
 
 export default function Routers() {
   return (
-    <Routes>
-      <Route index element={<Navigate replace to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/shop/:id" element={<ProductDetails />}>
-        <Route index element={<Navigate replace to="description" />} />
-        <Route path="description" element={<Description />} />
-        <Route path="reviews" element={<Reviews />} />
-      </Route>
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/checkout"
-        element={
-          <ProtectRoute>
-            <Checkout />
-          </ProtectRoute>
-        }
-      />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <Suspense fallback={<SpinnerFullPage />}>
+      <Routes>
+        <Route index element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<ProductDetails />}>
+          <Route index element={<Navigate replace to="description" />} />
+          <Route path="description" element={<Description />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/checkout"
+          element={
+            <ProtectRoute>
+              <Checkout />
+            </ProtectRoute>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Suspense>
   );
 }
