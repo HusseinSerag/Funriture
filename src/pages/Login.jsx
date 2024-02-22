2753f5bda3e0745cdd977360801774681e09dca8
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-02-22 20:47:39 +0200

    Create Register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,45 @@
+import { Col, Container, Form, FormGroup, Row } from "react-bootstrap";
+import styles from "./../styles/Login.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function Login() {
-  return <div>login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h1 className={styles.title}>Login</h1>
+
+              <Form className={styles.authForm}>
+                <FormGroup>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <button className={styles.btn}>Login</button>
+                <p>
+                  Don&apos;t have an account?{" "}
+                  <Link to="/register">Create an account</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,61 @@
+import { Col, Container, Form, FormGroup, Row } from "react-bootstrap";
+import styles from "./../styles/Register.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function Register() {
-  return <div>register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [files, setFiles] = useState(null);
+  return (
+    <div>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h1 className={styles.title}>Register</h1>
+
+              <Form className={styles.authForm}>
+                <FormGroup>
+                  <input
+                    type="text"
+                    placeholder="Enter your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <input
+                    type="file"
+                    onChange={(e) => setFiles(e.target.files[0])}
+                  />
+                </FormGroup>
+                <button className={styles.btn}>Register</button>
+                <p>
+                  Already have an account?{" "}
+                  <Link to="/login">Login into account</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
 }
